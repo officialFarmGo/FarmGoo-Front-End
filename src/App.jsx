b9bae58e81c3b2d9cd75ec794efd6220d51dc09c
unknown <clintoncute04@gmail.com>
2026-06-05 11:46:55 +0100

    fix: explicit case path matching for production buildimgss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import LandingPageComponents from './Components/UI/LandingPageComponents';
 import LoginPage from './Pages/Auth/LoginPage';
 import SignupPage from './Pages/Auth/SignPage';
 import DashboardPagesComponent from './Components/UI/DashboardPagesComponent';
 import VerificationOtp from './Pages/Auth/VerificationOtp';
 import SuccessFullVerification from './Pages/Auth/SuccessFullVerification';
-import FarmersDahboard from './Pages/FarmerDahboard/Dashboard'
+import FarmersDahboard from './Pages/FarmerDahboard/Dashboard';
 import DriverDashboard from './Pages/DriverDashBoard/DashBoard';
 import AgentDashboard from './Pages/AgentDasboard/DashBoard';
 import Wallet from './Components/Wallet';
@@ -20,43 +20,40 @@ import { AuthProvider } from './Context/AuthContext';
 import DriverDashboardView from './Components/UI/DriverDashboardView';
 import Info from './Pages/Auth/Info';
 import Notification from './Components/Notification';
+
 function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPageComponents />} />
-        <Route path="/login" element={<LoginPage/> } />
-        <Route path="/signup" element={<SignupPage/> } />
-        <Route path='/otp'  element={<VerificationOtp/>}/>
-        <Route path='/chooseDash' element={<Info/>}/>
-        <Route path="/success" element={<SuccessFullVerification/>}/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPageComponents />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/otp" element={<VerificationOtp />} />
+          <Route path="/chooseDash" element={<Info />} />
+          <Route path="/success" element={<SuccessFullVerification />} />
 
-          <Route path='/dashboard' element={<DashboardRedirect/>}/>
+          <Route path="/dashboard" element={<DashboardRedirect />} />
 
+          <Route path="/farmer/dashboard" element={<FarmersDahboard />}>
+            <Route path="" element={<DashboardPagesComponent />} />
+            <Route path="wallet" element={<Wallet />} />
+            <Route path="notification" element={<Notification />} />
+            <Route path="settings" element={<ProfileSettingsDashboard />} />
+            <Route path="activedelivery" element={<ActiveDeliveryPageComponets />} />
+          </Route>
 
-        <Route path='/farmer/dashboard' element={<FarmersDahboard/>}>
-          <Route path='' element={<DashboardPagesComponent/>}/>
-          <Route path='wallet' element={<Wallet/>}/>
-          <Route path='notification' element={<Notification/>}/>
-          <Route path='settings' element={<ProfileSettingsDashboard/>}/>
-          <Route path='activedelivery' element={<ActiveDeliveryPageComponets/>}/>
-        </Route>
+          <Route path="/agent/dashboard" element={<AgentDashboard />}>
+            <Route path="" element={<DashboardPagesComponent />} />
+          </Route>
 
-
-        <Route path='/agent/dashboard' element={<AgentDashboard/>}>
-          <Route path='' element={<DashboardPagesComponent/>}/>
-        </Route>
-
-
-        <Route path='/drivers/dashboard' element={<DriverDashboard/>}>
-          <Route path='' element={<DriverDashboardView/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/drivers/dashboard" element={<DriverDashboard />}>
+            <Route path="" element={<DriverDashboardView />} />
+          </Route>
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
+
 export default App;
-
-
